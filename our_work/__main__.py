4da7610272595549b972a941abb32fb773b8f05b
Kowalski2332 <frederik_kowalski@hotmail.com>
2024-10-27 09:35:24 +0100

    petri net klasse og metoder

--- our_work/__main__.py
+++ our_work/__main__.py
@@ -1,23 +1,25 @@
 from EventLog import EventLog
 from GraphBuilder import GraphBuilder
+from PetriNet import PetriNet, Place, Transition, Arc
+from pm4py.visualization.petri_net import visualizer as pn_visualizer
+from pm4py.analysis import check_soundness, check_is_workflow_net
+
 
 if __name__ == "__main__":
-    file_name = "data.xes"
-    eventlog = EventLog.load_xes(file_name)
+    # Create simple example of petri and visualize it
+    petri_net = PetriNet(
+        places=[Place("p1", 1), Place("p2", 0), Place("p3", 0)],
+        arcs=[Arc("p1", "t1"), Arc("t1", "p2"), Arc("p2", "t2"), Arc("t2", "p3"), Arc("p1", "t3"), Arc("t3", "p3"), Arc("p2", "t3")],
+        transitions=[Transition("t1"), Transition("t2"), Transition("t3")]
+    )
     
-    # Build a Petri net graph
-    graph_builder = GraphBuilder()
-    petrinet_graph = graph_builder.build_trace_graph(eventlog)
+    # petri_net.visualize("./example/petri_net")
+    pm4py_net, init, end = petri_net.to_pm4py()
     
-    print(petrinet_graph)    
+    # visualize the pm4py petri net
+    gviz = pn_visualizer.apply(pm4py_net, init, end)
+    pn_visualizer.view(gviz)
     
-    # Visualize the graph
-    graph_builder.visualize_graph(petrinet_graph)
-    
-    
-    
-    
-    
-    
-    
-        
+    # print(petri_net.soundness_check())
+    # print(petri_net.easy_soundness_check())
+    # print(petri_net.connectedness_check())

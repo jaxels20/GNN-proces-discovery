0eca6e2f19b4d551a99147017a8ae8f9b39a9422
Kowalski2332 <frederik_kowalski@hotmail.com>
2024-11-18 21:17:18 +0100

    ai lab bash script

--- our_work/ai_lab_training.sh
+++ our_work/ai_lab_training.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=model_training
 #SBATCH --output=result_%j.out
-#SBATCH --error=error_%j.err
 #SBATCH --ntasks=1
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 
-PYTHON_SCRIPT="training.py"
-VIRTUAL_ENVIRONMENT_NAME="project"
+PYTHON_SCRIPT_PATH="training.py"
 USER="fkowal20"
-CONTAINER_NAME="pytorch_24.09.sif"
 
-PROJECT_PATH="/ceph/home/student.aau.dk/$USER/$VIRTUAL_ENVIRONMENT_NAME"
-CONTAINER_PATH="/ceph/container/pytorch/$CONTAINER_NAME"
-BIND_PATH="$PROJECT_PATH:/project"
-
-singularity exec --bind $BIND_PATH $CONTAINER_PATH $PYTHON $PYTHON_SCRIPT
+singularity exec /ceph/project/pytorch/pm-container.sif python3 training.py

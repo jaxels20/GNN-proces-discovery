7d053ed08af2ff2a2cffd92fcb9a8e304fbf658a
Kowalski2332 <frederik_kowalski@hotmail.com>
2024-12-19 15:40:48 +0100

    add data gen params as seperate files and other small changes

--- analyze_candidate_places.py
+++ analyze_candidate_places.py
@@ -1,16 +1,8 @@
 from src.CandidateAnalyzer import CandidateAnalyzer
-SCENARIO = "controlled"
 
 if __name__ == "__main__":
-    if SCENARIO == "controlled":
-        input_dir = "./controlled_scenarios/"
-        output_dir = "./candidate_analysis_results/controlled_scenarios/"
-        analyzer = CandidateAnalyzer(input_dir, output_dir)
-        analyzer.evaluate_on_controlled_scenarios()
-        print("Candidate analysis complete.")     
-    elif SCENARIO == "synthetic":
-        input_dir = "./data_generation/synthetic_data/train/"
-        output_dir = "./candidate_analysis_results/synthetic_data/"
-        analyzer = CandidateAnalyzer(input_dir, output_dir)
-        analyzer.evaluate_on_synthetic_data()
-        print("Candidate analysis complete.")
+    input_dir = "./controlled_scenarios/"
+    output_dir = "./candidate_analysis_results/controlled_scenarios/"
+    analyzer = CandidateAnalyzer(input_dir, output_dir)
+    analyzer.evaluate_on_controlled_scenarios()
+    print("Candidate analysis complete.")     
